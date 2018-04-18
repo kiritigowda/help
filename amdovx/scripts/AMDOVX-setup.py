ef2c9e537e07294d01843a4e3af58198596d54f3
Kiriti <kiritigowda@gmail.com>
2018-04-18 10:37:39 -0700

    Setup into user dir option added

--- amdovx/scripts/AMDOVX-setup.py
+++ amdovx/scripts/AMDOVX-setup.py
@@ -4,74 +4,82 @@ import sys
 import subprocess
  
 
-opts, args = getopt.getopt(sys.argv[1:], 's:')
+opts, args = getopt.getopt(sys.argv[1:], 's:d:')
  
 sudoPassword = ''
+setupDir = ''
 
 for opt, arg in opts:
     if opt == '-s':
         sudoPassword = arg
+    elif opt =='-d':
+    	setupDir = arg
 
 if sudoPassword == '':
-    print('Invalid command line arguments. -s [Sudo Password] ')
+    print('Invalid command line arguments. -s [sudo password - required] -d [setup directory - optional] ')
     exit()
 
+if setupDir == '':
+	setupDir_deps = '~/deps'
+else:
+	setupDir_deps = setupDir+'deps'
+
 from subprocess import call
-deps_dir = os.path.expanduser('~/deps')
+deps_dir = os.path.expanduser(setupDir_deps)
 
 # AMDOVX setup
 if(os.path.exists(deps_dir)):
 	print("\nAMDOVX Dependencies Installed\n")
 else:
 	print("\nAMDOVX Dependencies Installation\n")
-	os.system('(cd ; mkdir deps)');
-	os.system('(cd ~/deps; git clone https://github.com/RadeonOpenCompute/rocm-cmake.git )');
-	os.system('(cd ~/deps; git clone https://github.com/ROCmSoftwarePlatform/MIOpenGEMM.git )');
-	os.system('(cd ~/deps; git clone https://github.com/ROCmSoftwarePlatform/MIOpen.git )');
-	os.system('(cd ~/deps; git clone https://github.com/google/protobuf.git )');
-	os.system('(cd ~/deps; wget https://github.com/opencv/opencv/archive/3.3.0.zip )');
-	os.system('(cd ~/deps; unzip 3.3.0.zip )');
-	os.system('(cd ~/deps; mkdir build )');
-	os.system('(cd ~/deps/build; mkdir rocm-cmake MIOpenGEMM MIOpen OpenCV )');
-	os.system('(cd ~/deps/build/rocm-cmake; cmake ../../rocm-cmake )');
-	os.system('(cd ~/deps/build/rocm-cmake; make -j8 )');
-	cmd='(cd ~/deps/build/rocm-cmake; sudo -S make install )'
+	os.system('(cd '+setupDir+'; mkdir deps)');
+	os.system('(cd '+deps_dir+'; git clone https://github.com/RadeonOpenCompute/rocm-cmake.git )');
+	os.system('(cd '+deps_dir+'; git clone https://github.com/ROCmSoftwarePlatform/MIOpenGEMM.git )');
+	os.system('(cd '+deps_dir+'; git clone https://github.com/ROCmSoftwarePlatform/MIOpen.git )');
+	os.system('(cd '+deps_dir+'; git clone https://github.com/google/protobuf.git )');
+	os.system('(cd '+deps_dir+'; wget https://github.com/opencv/opencv/archive/3.3.0.zip )');
+	os.system('(cd '+deps_dir+'; unzip 3.3.0.zip )');
+	os.system('(cd '+deps_dir+'; mkdir build )');
+	os.system('(cd '+deps_dir+'/build; mkdir rocm-cmake MIOpenGEMM MIOpen OpenCV )');
+	os.system('(cd '+deps_dir+'/build/rocm-cmake; cmake ../../rocm-cmake )');
+	os.system('(cd '+deps_dir+'/build/rocm-cmake; make -j8 )');
+	cmd='(cd '+deps_dir+'/build/rocm-cmake; sudo -S make install )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	os.system('(cd ~/deps/build/MIOpenGEMM; cmake ../../MIOpenGEMM )');
-	os.system('(cd ~/deps/build/MIOpenGEMM; make -j8 )');
-	cmd='(cd ~/deps/build/MIOpenGEMM; sudo -S make install )'
+	os.system('(cd '+deps_dir+'/build/MIOpenGEMM; cmake ../../MIOpenGEMM )');
+	os.system('(cd '+deps_dir+'/build/MIOpenGEMM; make -j8 )');
+	cmd='(cd '+deps_dir+'/build/MIOpenGEMM; sudo -S make install )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/MIOpen; sudo -S cmake -P install_deps.cmake )'
+	cmd='(cd '+deps_dir+'/MIOpen; sudo -S cmake -P install_deps.cmake )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/build/MIOpen; sudo -S apt-get install libssl-dev libboost-dev libboost-system-dev libboost-filesystem-dev  )'
+	cmd='(cd '+deps_dir+'/build/MIOpen; sudo -S apt-get install libssl-dev libboost-dev libboost-system-dev libboost-filesystem-dev  )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	os.system('(cd ~/deps/build/MIOpen; cmake -DMIOPEN_BACKEND=OpenCL ../../MIOpen )');
-	os.system('(cd ~/deps/build/MIOpen; make -j8 )');
-	os.system('(cd ~/deps/build/MIOpen; make MIOpenDriver )');
-	cmd='(cd ~/deps/build/MIOpen; sudo -S make install )'
+	os.system('(cd '+deps_dir+'/build/MIOpen; cmake -DMIOPEN_BACKEND=OpenCL ../../MIOpen )');
+	os.system('(cd '+deps_dir+'/build/MIOpen; make -j8 )');
+	os.system('(cd '+deps_dir+'/build/MIOpen; make MIOpenDriver )');
+	cmd='(cd '+deps_dir+'/build/MIOpen; sudo -S make install )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/build/MIOpen; sudo -S apt autoremove )'
+	cmd='(cd '+deps_dir+'/build/MIOpen; sudo -S apt autoremove )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/build/MIOpen; sudo -S apt autoclean )'
+	cmd='(cd '+deps_dir+'/build/MIOpen; sudo -S apt autoclean )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/protobuf; sudo -S apt-get install autoconf automake libtool curl make g++ unzip )'
+	cmd='(cd '+deps_dir+'/protobuf; sudo -S apt-get install autoconf automake libtool curl make g++ unzip )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/protobuf; sudo -S apt autoremove )'
+	cmd='(cd '+deps_dir+'/protobuf; sudo -S apt autoremove )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/protobuf; sudo -S apt autoclean )'
+	cmd='(cd '+deps_dir+'/protobuf; sudo -S apt autoclean )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	os.system('(cd ~/deps/protobuf; git submodule update --init --recursive )');
-	os.system('(cd ~/deps/protobuf; ./autogen.sh )');
-	os.system('(cd ~/deps/protobuf; ./configure )');
-	os.system('(cd ~/deps/protobuf; make -j16 )');
-	os.system('(cd ~/deps/protobuf; make check -j16 )');
-	cmd='(cd ~/deps/protobuf; sudo -S make install )'
+	os.system('(cd '+deps_dir+'/protobuf; git submodule update --init --recursive )');
+	os.system('(cd '+deps_dir+'/protobuf; ./autogen.sh )');
+	os.system('(cd '+deps_dir+'/protobuf; ./configure )');
+	os.system('(cd '+deps_dir+'/protobuf; make -j16 )');
+	os.system('(cd '+deps_dir+'/protobuf; make check -j16 )');
+	cmd='(cd '+deps_dir+'/protobuf; sudo -S make install )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/protobuf; sudo -S ldconfig )'
+	cmd='(cd '+deps_dir+'/protobuf; sudo -S ldconfig )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	os.system('(cd ~/deps/build/OpenCV; cmake -DWITH_OPENCL=OFF -DWITH_OPENCLAMDFFT=OFF -DWITH_OPENCLAMDBLAS=OFF -DWITH_VA_INTEL=OFF -DWITH_OPENCL_SVM=OFF ../../opencv-3.3.0 )');
-	os.system('(cd ~/deps/build/OpenCV; make -j8 )');
-	cmd='(cd ~/deps/build/OpenCV; sudo -S make install )'
+	os.system('(cd '+deps_dir+'/build/OpenCV; cmake -DWITH_OPENCL=OFF -DWITH_OPENCLAMDFFT=OFF -DWITH_OPENCLAMDBLAS=OFF -DWITH_VA_INTEL=OFF -DWITH_OPENCL_SVM=OFF ../../opencv-3.3.0 )');
+	os.system('(cd '+deps_dir+'/build/OpenCV; make -j8 )');
+	cmd='(cd '+deps_dir+'/build/OpenCV; sudo -S make install )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
-	cmd='(cd ~/deps/build/OpenCV; sudo -S ldconfig )'
+	cmd='(cd '+deps_dir+'/build/OpenCV; sudo -S ldconfig )'
 	call('echo {} | {}'.format(sudoPassword, cmd), shell=True)
